31d5aca02448c3c27b40a8996dfb8cb2a057a4a6
sanjeewa03 <sanjeewasenarathna03@gmail.com>
2019-08-23 10:31:48 +0530

    display input

--- src/app/Homepage/sign-in/sign-in.component.ts
+++ src/app/Homepage/sign-in/sign-in.component.ts
@@ -11,37 +11,36 @@ export class SignInComponent  {
 
  // showSucessMessage: boolean;
 
-
+  values = '';
   clickMessage = '';
 
-
+  onKey(event: any ) {
+    this.values = event.target.value;
+  }
     onClickMe(f: NgForm) {
-      //this.clickMessage = 'Successful';
+      // this.clickMessage = 'Successful';
       console.log(f.value.first);
       if ( !f.value.first ) {
 
         this.clickMessage = 'username can not be empty';
-      }
-      else{
+      } else {
       console.log(f.value.email);
       if
        ( !f.value.email ) {
 
         this.clickMessage = 'Email can not be empty';
-      }
-     else {
-        if(!f.value.pwd){
+      } else {
+        if ( !f.value.pwd ) {
           this.clickMessage = 'password can not be empty';
-        }
-        else{
+        } else {
           this.clickMessage = 'Successful';
         }
 
       }
 
 
-  //this.showSucessMessage = true;
-  //setTimeout(()=> this.showSucessMessage = false, 4000);
+  // this.showSucessMessage = true;
+  // setTimeout(()=> this.showSucessMessage = false, 4000);
     }
   }
 }
